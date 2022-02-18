dd2d42b48312d8252672faa4f9e67db87877c055
SomeduttaSahoo <90901367+SomeduttaSahoo@users.noreply.github.com>
2022-02-18 07:21:33 +0530

    Update check_limits.py

--- check_limits.py
+++ check_limits.py
@@ -1,18 +1,50 @@
 
-def battery_is_ok(temperature, soc, charge_rate):
-  if temperature < 0 or temperature > 45:
-    print('Temperature is out of range!')
+def PrintoutofRange(parameter,value):
+  print(parameter ,'with the reading',value,'is out of the range!')
+  
+  
+def isTemperatureInRange(temperature):
+  temperatur=convertTocelsius(temperature)
+  if temperature < 0 :
     return False
-  elif soc < 20 or soc > 80:
-    print('State of Charge is out of range!')
+  elif temperature > 45 :
     return False
-  elif charge_rate > 0.8:
-    print('Charge rate is out of range!')
+  else:
+    return True
+  
+def issocInRange(soc):
+  if soc < 20 :
     return False
+  elif soc > 80 :
+    return False
+  else:
+    return True
+  
+def ischarge_rateInRange(charge_rate):
+  if charge_rate > 0.8:
+    return False
+  else:
+    return True
+ 
+def convertTocelsius(temperature):
+  if('f' in temperature.lower() ):
+    return((int(temperature.lower().split('f')[0])-32.0)*(5.0/9.0))
+  elif('k' in temperature.lower()):
+    return(int(temperature.lower().split('k')[0])-273.0)
+  else:
+    return(int(temperature.lower().split('c')[0]))
 
-  return True
+  
+def battery_is_ok(temperature, soc, charge_rate):
+Checktemperature=isTemperatureInRange(temperature)
+Checksoc=issocInRange(soc)
+Checkcharge_rate=ischarge_rateInRange(charge_rate)
+  
+return Checktemperature and Checksoc and Checkcharge_rate
 
 
 if __name__ == '__main__':
-  assert(battery_is_ok(25, 70, 0.7) is True)
-  assert(battery_is_ok(50, 85, 0) is False)
+  assert(battery_is_ok("25c", 70, 0.7) is True)
+  assert(battery_is_ok("50c", 85, 0) is False)
+  assert(battery_is_ok("100f", 85, 0) is False)
+  assert(battery_is_ok("271k", 85, 0) is False)
